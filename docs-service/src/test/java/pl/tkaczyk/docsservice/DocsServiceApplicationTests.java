ab13871a68f4856fcca53f67b65cf31cf0d79d5d
patryktk <patrick.tkaczyk@gmail.com>
2025-04-06 15:47:37 +0200

    Initial commit

---
+++ docs-service/src/test/java/pl/tkaczyk/docsservice/DocsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tkaczyk.docsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
